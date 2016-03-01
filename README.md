aadcae992446e28c1761dfaba48d042fd0194d4f
Lee Brooks <lee@kagiso.io>
2016-03-01 09:17:50 +0200

    Corrected setup.py file

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Notify Slack via a web hook when a page has been published
 ## Installation
 `pip install kagiso_wagtail_slack`
 
-Add your Slack webhook as SLACK_PAGE_PUBLISHED_WEBHOOK in your settings.py
+Add your Slack webhook as `SLACK_PAGE_PUBLISHED_WEBHOOK` in your settings.py
 
 Add `kagiso_wagtail_slack.apps.KagisoWagtailSlackConfig` to your `INSTALLED_APPS` in your `settings.py`.
 
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='kagiso_wagtail_sitemap',
+    name='kagiso_wagtail_slack',
     version='0.0.1',
     author='Kagiso Media',
     author_email='development@kagiso.io',
-    description='Kagiso Search',
-    url='https://github.com/Kagiso-Future-Media/kagiso_wagtail_sitemap',
+    description='Kagiso Wagtail Slack',
+    url='https://github.com/Kagiso-Future-Media/kagiso_wagtail_slack',
     packages=find_packages(),
     install_requires=[
         'wagtail>=1.3.1',
