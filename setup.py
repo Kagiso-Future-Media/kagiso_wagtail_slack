c73f4f9c1476c67d2e28c54df797d74912871482
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2017-02-02 11:23:09 +0200

    Upgrade to Django 1.01.5 and Wagtail 1.6.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_wagtail_slack',
-    version='0.0.4',
+    version='1.0.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Wagtail Slack',
     url='https://github.com/Kagiso-Future-Media/kagiso_wagtail_slack',
     packages=find_packages(),
     install_requires=[
-        'wagtail>=1.3.1',
+        'wagtail>=1.6.3',
     ]
 )
