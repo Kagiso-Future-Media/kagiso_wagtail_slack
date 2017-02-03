fcdd21b8801fb43b7d28b18e4b44e3f7faf48d1f
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2017-02-03 15:14:35 +0200

    Updated to Wagtail 1.8.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_wagtail_slack',
-    version='1.0.0',
+    version='2.0.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Wagtail Slack',
     url='https://github.com/Kagiso-Future-Media/kagiso_wagtail_slack',
     packages=find_packages(),
     install_requires=[
-        'wagtail>=1.6.3',
+        'wagtail>=1.8.0',
     ]
 )
